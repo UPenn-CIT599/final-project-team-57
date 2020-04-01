53ee6ff377bd98bc09f06c210e67b27e743c9b14
SamanthaPace <62861475+SamanthaPace@users.noreply.github.com>
2020-04-01 15:09:39 -0500

    Update DataAnalysis.java

--- src/DataAnalysis.java
+++ src/DataAnalysis.java
@@ -1,4 +1,78 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class DataAnalysis {
+	
+	ArrayList<HotelBooking> hotelBookings = new ArrayList<HotelBooking>();
+	
+	public DataAnalysis (ArrayList<HotelBooking> hotelBookings) {
+		this.hotelBookings = hotelBookings;
+	}
+
+
+	/**
+	 * Returns all hotel booking objects read from CSV
+	 * @return
+	 */
+	public ArrayList<HotelBooking> getHotelBooking() {
+		return hotelBookings;
+	}
+
+	/**
+	 * Question 1
+	 * 	Finds the most and least busy time to book a hotel room
+	 * @return
+	 */
+	
+	public String getAnswer1(){
+		HashMap<String, Integer> BestTimeToBook = new HashMap<>();
+		for  (HotelBooking hotelBooking : hotelBookings) {
+			if  (!hotelBooking.getArrivalDateMonth().equals("")  && hotelBooking.getArrivalDateDayOfMonth() > 0) {
+				
+				//converts the int (day) to a string minus comma and back to the int
+				String day = Integer.toBinaryString(hotelBooking.getArrivalDateDayOfMonth());
+				String dayString = day.substring(0, day.length() -1);
+				int arrivialDateDayOfMonth = Integer.parseInt(dayString);
+				
+				// count starts at one due to the else statement being the first instance 
+				int count = 1;
+				if (BestTimeToBook.containsKey(hotelBooking.getArrivalDateMonth()) && BestTimeToBook.containsValue(arrivialDateDayOfMonth)) {
+					count++;
+				}
+				else {
+					BestTimeToBook.put(hotelBooking.getArrivalDateMonth(), arrivialDateDayOfMonth);
+				}
+			}
+			
+		}
+	
+		String BestBookingTime = "";
+		String WorstBookingTime = "";
+
+		ArrayList<Integer> numberOfBookings = new ArrayList<>();
+		for (String ArrivalDateMonth : BestTimeToBook.keySet()) {
+			numberOfBookings.add(BestTimeToBook.get(ArrivalDateMonth));
+		}
+		
+
+		//sort low to high, then iterate to see what item matches lowest number of bookings
+		Collections.sort(numberOfBookings);
+		for (String ArrivalDateMonth : BestTimeToBook.keySet()) {
+			if (BestTimeToBook.get(ArrivalDateMonth) == numberOfBookings.get(0)) {
+				BestBookingTime = ArrivalDateMonth;
+			}
+		}
+		for (String ArrivalDateMonth : BestTimeToBook.keySet()) {
+			if (BestTimeToBook.get(ArrivalDateMonth) == numberOfBookings.get(numberOfBookings.size() - 1)) {
+				WorstBookingTime = ArrivalDateMonth;
+			}
+		}
+		
+		
+		String answer1 =  "Best time to book is " + BestBookingTime + "Worst time to book is " + WorstBookingTime;
+
+		return answer1;
+	}
 
 }
