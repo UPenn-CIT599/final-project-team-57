6fb88426d2d491c4a35c6b11c63aaf00ef2ede01
Pace <samantha.pace@bms.com>
2020-04-20 17:13:23 -0400

    add the runner data for all Q&A

--- src/main/java/DataAnalysis.java
+++ src/main/java/DataAnalysis.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -8,10 +10,34 @@ public class DataAnalysis {
 
 	List<HotelBooking> hotelBookings = new ArrayList<HotelBooking>();
 
-	public DataAnalysis (List<HotelBooking> hotelBookings) {
-		this.hotelBookings = hotelBookings;
+	public DataAnalysis (List<HotelBooking> hotelBookingList) {
+		this.hotelBookings = hotelBookingList;
 	}
-
+	
+	/**
+	 * Returns all hotel booking objects read from CSV
+	 * @return
+	 */
+	public List<HotelBooking> getHotelBooking() {
+		return hotelBookings;
+	}
+	
+	
+	/**
+	 * Finds all cancelled rooms
+	 * @return
+	 */
+	public ArrayList<HotelBooking> isCanceled() {
+		ArrayList<HotelBooking> results = new ArrayList<HotelBooking>();
+		for (HotelBooking canceled : hotelBookings) {
+			if (canceled.isCanceled()) {
+				results.add(canceled);
+			}
+		}
+		return results;
+	}
+	
+	
 	/**
 	 * Question 1: What is the maximum rate for a non-cancelled resort hotel room in USA over the years?
 	 * @return
@@ -57,38 +83,55 @@ public class DataAnalysis {
 	 * @return
 	 */
 
-	public static double getCancelledPercentage(ArrayList<HotelBooking> cancelled) {
-		double totalReservations = 0;
+	public double getAnswer3(ArrayList<HotelBooking> isCanceled) {
+		double percentage = 0.0;
+		double totalReservations = 0.0;
 		int count = 0;
-		for (HotelBooking hotelBooking : cancelled) {
+	
+		for (HotelBooking hotelBooking : isCanceled) {
 			if (hotelBooking.isCanceled()) { // Rooms without cancellation info have a value  = -1 and are not included
 				totalReservations++;
 				count++;
 			}
-			else {
+			else   { // Rooms without cancellation info have a value  = -1 and are not included
 				totalReservations++;
 			}
 		}
-		return (count / totalReservations) * 100;
+
+		percentage = ((count / totalReservations) * 100); // converting to percentage 
+		System.out.println("Percentage of rooms canceled = " + percentage + "%");
+		return percentage;
+		
 	}
 
 	/**
 	 * Question 4: What is the average number of days stayed?
-	 * @param nightsStayed
+	 * @param hotelBooking2
 	 * @return
 	 */
 
-	public static double getAvgNightsStayed(ArrayList<HotelBooking> nightsStayed) {
-		double sum=0;
-		int count=0;
-		for (HotelBooking hotelBooking: nightsStayed) {
+	public double getAnswer4(List<HotelBooking> hotelBooking2) {
+		double sum = 0;
+		int count = 0;
+		double average = 0;
+		double roundedAverage = 0;
+		
+		for (HotelBooking hotelBooking: hotelBooking2) {
 			if(hotelBooking.getStaysInWeekNights()>=0 || hotelBooking.getStaysInWeekendNights()>=0 ) { //Rooms without rate info have a value  = -1 and are not included
 				sum += hotelBooking.getStaysInWeekNights();
 				sum += hotelBooking.getStaysInWeekendNights();
 				count++;
 			}
+		
 		}
-		return sum/count;
+		average = sum / count;
+		BigDecimal bd = new BigDecimal(average);
+		bd = bd.round(new MathContext(3));
+		roundedAverage = bd.doubleValue();
+		
+		System.out.println("The average number of days stayed at a hotel = " + roundedAverage);
+		
+		return roundedAverage;
 	}
 
 
@@ -97,15 +140,82 @@ public class DataAnalysis {
 	 * @return
 	 */
 
-	public static double getAvgLeadTime(ArrayList<HotelBooking> leadTime) {
-		double sum=0;
-		int count=0;
-		for (HotelBooking booking: leadTime) {
+	public double getAnswer5(List<HotelBooking> hotelBooking) {
+		double sum = 0;
+		int count = 0;
+		double average = 0;
+		double roundedAverage = 0;
+		
+		for (HotelBooking booking: hotelBooking) {
 			if(booking.getLeadTime()>=0) {//not void
 				sum += booking.getLeadTime();
 				count++;
 			}
 		}
-		return sum/count;
-}
+		average = sum / count;
+		BigDecimal bd = new BigDecimal(average);
+		bd = bd.round(new MathContext(3));
+		roundedAverage = bd.doubleValue();
+		
+		average = sum / count; 
+		System.out.println("The average days of lead time for booking a hotel room = " + roundedAverage);
+		
+		return roundedAverage;
+	}
+	
+	/**
+	 * Question 6
+	 * 	Finds the most and least busy time to book a hotel room
+	 * @return
+	 */
+	
+	public String getAnswer6(){
+		HashMap<String, Integer> BestTimeToBook = new HashMap<>();
+		for  (HotelBooking hotelBooking : hotelBookings) {
+			if  (!hotelBooking.getArrivalDateMonth().equals("")  && hotelBooking.getArrivalDateDayOfMonth() > 0) {
+				
+				//converts the int (day) to a string minus comma and back to the int
+				String day = Integer.toBinaryString(hotelBooking.getArrivalDateDayOfMonth());
+				String dayString = day.substring(0, day.length() -1);
+				int arrivialDateDayOfMonth = Integer.parseInt(dayString);
+				
+				// count starts at one due to the else statement being the first instance 
+				int count = 1;
+				if (BestTimeToBook.containsKey(hotelBooking.getArrivalDateMonth()) && BestTimeToBook.containsValue(arrivialDateDayOfMonth)) {
+					count++;
+				}
+				else {
+					BestTimeToBook.put(hotelBooking.getArrivalDateMonth(), arrivialDateDayOfMonth);
+				}
+			}
+			
+		}
+	
+		String BestBookingTime = "";
+		String WorstBookingTime = "";
+
+		ArrayList<Integer> numberOfBookings = new ArrayList<>();
+		for (String ArrivalDateMonth : BestTimeToBook.keySet()) {
+			numberOfBookings.add(BestTimeToBook.get(ArrivalDateMonth));
+		}
+		
+
+		//sort low to high, then iterate to see what item matches lowest number of bookings
+		Collections.sort(numberOfBookings);
+		for (String ArrivalDateMonth : BestTimeToBook.keySet()) {
+			if (BestTimeToBook.get(ArrivalDateMonth) == numberOfBookings.get(0)) {
+				BestBookingTime = ArrivalDateMonth;
+			}
+		}
+		for (String ArrivalDateMonth : BestTimeToBook.keySet()) {
+			if (BestTimeToBook.get(ArrivalDateMonth) == numberOfBookings.get(numberOfBookings.size() - 1)) {
+				WorstBookingTime = ArrivalDateMonth;
+			}
+		}
+		
+		
+		String answer6 =  "Best time to book is " + BestBookingTime + "Worst time to book is " + WorstBookingTime;
+
+		return answer6;
+	}
 }
--- src/main/java/HotelBookingRunner.java
+++ src/main/java/HotelBookingRunner.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class HotelBookingRunner {
+	
+
 	public static void main(String[] args) {
 		HotelBookingReader hbr = new HotelBookingReader();
 		List<HotelBooking> hotelBookingList = hbr.readCSVFile("hotel_bookings.csv");
+
 		System.out.println("Number of rows " +hotelBookingList.size());
 		DataAnalysis da = new DataAnalysis(hotelBookingList);
+		List<HotelBooking> hotelBooking = da.getHotelBooking();
+		ArrayList<HotelBooking> canceledRooms = da.isCanceled();
+		
+		
 		da.getAnswer1();
 		da.getAnswer2();
+		da.getAnswer3(canceledRooms);
+		da.getAnswer4(hotelBooking);
+		da.getAnswer5(hotelBooking);
 		
-		
+
 		//FormattedOutput fo = new FormattedOutput();
 	}
 }
